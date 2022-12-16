b6c8bd60e17c447a1cf5364ba1b6b715023942a5
Haley Anderson <hjquiner@gmail.com>
2022-12-15 21:18:10 -0800

    day 3 part 2 haley attempt

--- day_three/index.js
+++ day_three/index.js
@@ -1,13 +1,13 @@
 const readinput = require("../lib/readinput");
 
-function main() {
-    const elfPacks = readinput(process.argv[2]);
+const elfPacks = readinput(process.argv[2]);
+
+function findTotalPriority() { 
     let priorityTotal = 0;
     for (let line of elfPacks) {
         const divIndex = line.length / 2;
         const firstHalf = line.substring(0, divIndex);
         const secondHalf = line.substring(divIndex);
-        const compareArr = [];
         for (let charIndex = 0; charIndex < firstHalf.length; charIndex++) {
             const searchLetter = firstHalf[charIndex];
             if(secondHalf.indexOf(searchLetter) >= 0) {
@@ -17,12 +17,37 @@ function main() {
                 break;
             }
         }
-        console.log(priorityTotal);
     }
     return priorityTotal;
 }
 
-console.log(main());
+function findTotalBadgePriority() {
+    let priorityTotal = 0;
+    const remainingElfPacks = [...elfPacks];
+    while(remainingElfPacks.length > 0 && remainingElfPacks.length >= 3) {
+        const packs = [remainingElfPacks.shift(), remainingElfPacks.shift(), remainingElfPacks.shift()];
+        let matchArr = packs[0].split('');
+        for(let pack of packs.slice(1)) {
+            for (let charIndex = 0; charIndex < pack.length; charIndex++) {
+                const searchLetter = pack[charIndex];
+                if(matchArr.indexOf(searchLetter) === -1){
+                    matchArr = matchArr.filter(e => e !== searchLetter);
+                }
+            }
+            if(matchArr.length === 1){
+                const letterPriority = prioritization(matchArr[0]);
+                priorityTotal += letterPriority;
+                break;
+            }
+        }
+    }
+    return priorityTotal;
+}
+
+//console.log(findTotalPriority());
+console.log(findTotalBadgePriority());
+
+
 
 function prioritization(letter) {
     const charNum = letter.charCodeAt(0);
